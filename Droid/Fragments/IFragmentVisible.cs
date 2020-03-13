0657e34ed1022c30dcb95d2ff1afb40f52522763
Pascal Raszyk <pascal@raszyk.de>
2020-03-13 22:35:14 +0100

    initial

---
+++ Droid/Fragments/IFragmentVisible.cs
@@ -0,0 +1,7 @@
+﻿namespace DontBugMe.Droid
+{
+    interface IFragmentVisible
+    {
+        void BecameVisible();
+    }
+}
---
+++ Droid/Helpers/RecyclerClickEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Android.Views;
+
+namespace DontBugMe.Droid
+{
+    public class RecyclerClickEventArgs : EventArgs
+    {
+        public View View { get; set; }
+        public int Position { get; set; }
+    }
+}
