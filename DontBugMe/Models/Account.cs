60ecc448d4833cfc9594bb70788336013f20692a
Jannik Löhr <j.loehr@live.de>
2020-03-14 00:26:03 +0100

    adds Account.cs + Message.cs

---
+++ DontBugMe/Models/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DontBugMe
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public string AppIdentifier { get; set; }
+
+        public List<Message> getMessages()
+        {
+            List<Message> messages = new List<Message>();
+            return messages;
+        }
+    }
+
+}
---
+++ DontBugMe/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DontBugMe
+{
+    public class Message
+    {
+        public string Type { get; set; }
+        public string Sender { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public bool Read { get; set; }
+        public int AccountId  { get; set; }
+
+    }
+
+}
