90b453424fd516ca908f50efd683e944d2dd16a2
Jannik Löhr <j.loehr@live.de>
2020-03-14 00:58:50 +0100

    adds method MarkAsRead to Message.cs

--- DontBugMe/Models/Message.cs
+++ DontBugMe/Models/Message.cs
@@ -4,13 +4,18 @@ namespace DontBugMe
 {
     public class Message
     {
+        public string Id { get; set; }
         public string Type { get; set; }
         public string Sender { get; set; }
         public string Subject { get; set; }
         public string Content { get; set; }
-        public bool Read { get; set; }
+        public bool IsRead { get; set; }
         public int AccountId  { get; set; }
 
+        public void MarkAsRead()
+        {
+            this.IsRead = true;
+            return;
+        }
     }
-
 }
